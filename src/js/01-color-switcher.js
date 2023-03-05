c7b60771234819f2ae36569a4150e8cfe757e551
momotvit <110504519+momotvit@users.noreply.github.com>
2023-03-05 15:14:56 -0800

    task 1 is completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+const documentBody = document.body;
 
+
+let colorChange = null;
+
+
+
+startBtn.addEventListener("click", onClickStart);
+stopBtn.addEventListener("click", onClickStop);
+
+
+stopBtn.setAttribute("disabled", "true");
+
+function onClickStart() { 
+   
+    colorChange = setInterval(bgColorChange, 1000);
+    startBtn.setAttribute("disabled", "true");
+    stopBtn.removeAttribute("disabled");
+}
+
+
+function onClickStop() { 
+    startBtn.removeAttribute("disabled");
+    clearInterval(colorChange);
+    stopBtn.setAttribute("disabled", "true");
+}
+
+
+function bgColorChange() {
+    documentBody.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
